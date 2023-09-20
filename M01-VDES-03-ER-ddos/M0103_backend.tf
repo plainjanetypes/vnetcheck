443b0c4945fe35e506dfab4f8afdc4e063fe003c
TANUSHAR <tanushri.sharma@accenture.com>
2023-09-20 19:42:12 +0530

    backend

--- M01-VDES-03-ER-ddos/M0103_backend.tf
+++
@@ -1,7 +0,0 @@
-#store state in terra general
-backend "azurerm" {
-    resource_group_name = ""
-    storage_account_name = "terrageneral"
-    container_name = "tfbackup"
-    key = "M01-VDES-03-ER-ddos.tfstate"
-}
